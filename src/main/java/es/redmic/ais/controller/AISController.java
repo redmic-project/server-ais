180a8e05e532990380e3b234ad059aa804f42ca0
Noel Alonso <noel@redmic.es>
2018-07-06 10:22:14 +0100

    Desactiva schedule en base a propiedad de spring
    De este modo solo se activará la descarga de datos en entorno de
    producción

--- src/main/java/es/redmic/ais/controller/AISController.java
+++ src/main/java/es/redmic/ais/controller/AISController.java
@@ -1,6 +1,12 @@
 package es.redmic.ais.controller;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,8 +14,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import es.redmic.ais.service.AISService;
 
 @Configuration
+@ConditionalOnProperty(value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true)
 @EnableScheduling
 public class AISController {
+
+	private static Logger logger = LogManager.getLogger();
+
+	@Value("${config.fixedDelay}")
+	Long fixedDelay;
 
 	@Autowired
 	AISService aisService;
@@ -17,6 +29,9 @@ public class AISController {
 	@Scheduled(fixedDelayString = "${config.fixedDelay}", initialDelayString = "${config.fixedDelay}")
 	public void execute() {
 
-		aisService.update();
+		logger.info(
+				"Descargando datos, próxima descarga en " + TimeUnit.MILLISECONDS.toSeconds(fixedDelay) + " segundos.");
+
+		aisService.fetchData();
 	}
 }
